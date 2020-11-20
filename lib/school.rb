60c13dd244ef053b41072c8dc502b700423a6088
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-20 08:55:12 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require "pry"
+class School
+   attr_reader :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def add_student(name,grade)
+        if roster[grade] 
+            roster[grade].push(name)
+        else 
+            new_arr = [name]
+            roster[grade] = new_arr 
+        end
+    end
+    def grade(i)
+        roster[i]
+    end
+    def sort
+        sorter = {}
+        roster.each do |grade, student|
+            sorter[grade] = student.sort 
+        end
+        sorter
+    end
+end
